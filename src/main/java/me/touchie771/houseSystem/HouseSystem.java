e49a00fc40bca806fececd4c188fe6c6dccd3d03
Touchie771 <mihai.constantin.bara@gmail.com>
2025-10-26 11:49:53 +0200

    initial commit

---
+++ src/main/java/me/touchie771/houseSystem/HouseSystem.java
@@ -0,0 +1,17 @@
+package me.touchie771.houseSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HouseSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
